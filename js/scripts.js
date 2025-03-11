c8b65aa08988631078a8802875cc3f844045b1c0
Piotr Kania <piotr@trener-it.pl>
2025-03-11 11:59:50 +0100

    Add responsive nav

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,14 @@
 "use strict";
+
+const modCss = (event) => {
+   const scrollY =  window.scrollY;
+   const nav = document.querySelector('.nav');
+
+   if(scrollY >= 200) {
+      nav.style.backgroundColor = 'red';
+   } else {
+      nav.style.backgroundColor = 'transparent';
+   }
+}
+
+window.addEventListener('scroll', modCss);
