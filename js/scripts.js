389b9b1f4463fa3723271e10a8ced0e40de53e9a
Piotr Kania <piotr@trener-it.pl>
2025-03-18 11:19:22 +0100

    Update styles and add mixins for responsive design

--- js/scripts.js
+++ js/scripts.js
@@ -3,9 +3,10 @@
 const modCss = (event) => {
    const scrollY =  window.scrollY;
    const nav = document.querySelector('.nav');
+   const bgColor = '#5A6E73';
 
    if(scrollY >= 200) {
-      nav.style.backgroundColor = 'red';
+      nav.style.backgroundColor = bgColor;
    } else {
       nav.style.backgroundColor = 'transparent';
    }
